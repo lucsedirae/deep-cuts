2c884a9295afbfb418698703d77b36093e2e0144
David Stinnett <davidstinnett@icloud.com>
2020-09-27 16:00:00 -0400

    commented out api JS file to stop interference with main JS file

--- assets/script/api-calls.js
+++ assets/script/api-calls.js
@@ -13,22 +13,22 @@
 //call url https://musicbrainz.org/ws/2/
 //docs https://musicbrainz.org/doc/MusicBrainz_API/Search
 //musicbrainz api call
-$.ajax({
-    url: "http://musicbrainz.org/ws/2/artist/?query=artist:"+artistName+"&fmt=json",
-    method: "GET"
-}).then(function(results){
-    //Index of results.artists can be iterated through at a later date to improve dynamics
-    var resArt = results.artists[0];
+// $.ajax({
+//     url: "http://musicbrainz.org/ws/2/artist/?query=artist:"+artistName+"&fmt=json",
+//     method: "GET"
+// }).then(function(results){
+//     //Index of results.artists can be iterated through at a later date to improve dynamics
+//     var resArt = results.artists[0];
 
-    artistObj = {
-        artist: resArt['name'],
-        activeFrom: resArt['life-span'].begin,
-        activeTo: resArt['life-span'].end,
-        genre: resArt.tags[0]
-    }
-    console.log(results);
-    console.log(artistObj);
-});
+//     artistObj = {
+//         artist: resArt['name'],
+//         activeFrom: resArt['life-span'].begin,
+//         activeTo: resArt['life-span'].end,
+//         genre: resArt.tags[0]
+//     }
+//     console.log(results);
+//     console.log(artistObj);
+// });
 
 
 
