0ca3cbba703decd6e64d16600310391cf2dc3b4d
LucSedirae <jondeavers@gmail.com>
2020-09-28 23:10:58 -0400

    API commenting

--- assets/script/api-calls.js
+++ assets/script/api-calls.js
@@ -1,6 +1,8 @@
 var artistName = "nirvana";
 var artistList = [];
 var artistObj = {};
+
+//BANDSINTOWN API
 //bandsintown api url, api key & documentation link
 //call url https://rest.bandsintown.com/artists/{{artist_name}}/?app_id=yOUrSuP3r3ven7aPp-id
 //docs https://artists.bandsintown.com/support/public-api?_ga=2.110307469.924392.1601057589-666678079.1600528655
@@ -8,7 +10,7 @@ var artistObj = {};
 
 
 
-
+//MUSICBRAINZ API
 //musicbrainz documentation link and call url (no api key required)
 //call url https://musicbrainz.org/ws/2/
 //docs https://musicbrainz.org/doc/MusicBrainz_API/Search
@@ -33,6 +35,8 @@ $.ajax({
 
 
 
+//YOUTUBE API
+//Google credentials API key: AIzaSyAWvi6Cb4U2R4VzJSEPftX7y3xVUJESaIw
 
 
 
@@ -49,8 +53,7 @@ $.ajax({
 
 
 
-
-
+//SPOTIFY/SHAZAM API
 //spotify api url
 //https://api.spotify.com/
 
