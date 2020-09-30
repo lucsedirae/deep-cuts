79c4e9d00eafaa325f52792a33dbbd3e7248afb8
David Stinnett <davidstinnett@icloud.com>
2020-09-29 22:05:57 -0400

    got results from youtube api

--- assets/script/api-calls.js
+++ assets/script/api-calls.js
@@ -43,33 +43,39 @@ function callMusicBrainzAPI() {
 //Google credentials API key: AIzaSyAWvi6Cb4U2R4VzJSEPftX7y3xVUJESaIw
 //<script src="https://apis.google.com/js/api.js"></script>//
 
-
-function authenticate() {
-  return gapi.auth2
-    .getAuthInstance()
-    .signIn({ scope: "https://www.googleapis.com/auth/youtube.force-ssl" })
-    .then(
-      function () {
-        console.log("Sign-in successful");
-      },
-      function (err) {
-        console.error("Error signing in", err);
-      }
-    );
-}
-function loadClient() {
-  gapi.client.setApiKey("YOUR_API_KEY");
-  return gapi.client
-    .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-    .then(
-      function () {
-        console.log("GAPI client loaded for API");
-      },
-      function (err) {
-        console.error("Error loading GAPI client for API", err);
-      }
-    );
-}
+function youtubeAPI () {
+  $.ajax({
+    url: "https://www.googleapis.com/youtube/v3/search?q=nirvana&key=AIzaSyAYrNxKe4mIXCg9zDRqt9hw6wT8fW6oGYc",
+    method: "GET"
+}).then(function (response) {
+  console.log(response)
+})}
+// function authenticate() {
+//   return gapi.auth2
+//     .getAuthInstance()
+//     .signIn({ scope: "https://www.googleapis.com/auth/youtube.force-ssl" })
+//     .then(
+//       function () {
+//         console.log("Sign-in successful");
+//       },
+//       function (err) {
+//         console.error("Error signing in", err);
+//       }
+//     );
+// }
+// function loadClient() {
+//   gapi.client.setApiKey("YOUR_API_KEY");
+//   return gapi.client
+//     .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+//     .then(
+//       function () {
+//         console.log("GAPI client loaded for API");
+//       },
+//       function (err) {
+//         console.error("Error loading GAPI client for API", err);
+//       }
+//     );
+// }
 // Make sure the client is loaded and sign-in is complete before calling this method.
 function execute() {
   return gapi.client.youtube.search
