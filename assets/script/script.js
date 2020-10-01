4cfb57665b5084a6fa17741fa1b187320c8d3657
LucSedirae <jondeavers@gmail.com>
2020-09-30 23:07:22 -0400

    Added additional nav functionality and dried out script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,37 +1,86 @@
 var artistHistory = [];
 
+//JD 9/30 Reorganized functions by alphabetical order to make searching them easier for reader
 //initialization function
 $(document).ready(function () {
   //calls function that appends default HTML to DOM
   populateMainSearch();
 
-  //clicker function for search button//
-  $(".search-btn").on("click", function (event) {
-    event.preventDefault();
-
-    //var currentArtistName stores user input
-    //NOTE: this var is declared and initially defined in the api-calls.js
-    currentArtistName = $(".searchTerm").val();
-    if (currentArtistName === "") {
-      return;
-    }
-    artistHistory.push(currentArtistName);
-    storeArtist();
-
-    //calling populateMainInfo()
-    //At this point populateInfoCard can be deprecated and so I am removing the function call and replacing
-    callMusicBrainzAPI();
-    callYoutubeAPI();
-    populateMainInfo();
-    $("#input").val("");
-    console.log(artistHistory);
-  });
-
+  //when called, it sets a set of event listeners in place allowing the nav icons to fucntion
   function activateListeners() {
     $("#youtube-btn").on("click", populateMainYoutube);
+    $("#info-btn").on("click", populateMainInfo);
+    $("#search-nav-btn").on("click", populateMainSearch);
+    $('#history-btn').on("click", populateMainHistory);
   }
 
-  // JD 9/29 created populateMenu() to reduce redundancy in other populate functions
+  //JD 9/30 Added function to populate artist history list
+  //populates main-content with a scrollable history list of previously searched artists
+  function populateMainHistory() {
+  $(".main-content").empty();
+  populateMenu();
+  activateListeners();
+  $(".main-content").append("<br><div class='col s4'></div><div class='col s4' id='history-list'>Testing</div>")
+  }
+
+    //JD 9/30 Moved menu population into it's own function to dry out code
+  //populateMainInfo replaces search html with Info html. Also called from nav icons
+  function populateMainInfo() {
+    $(".main-content").empty();
+    wikipediaSearch();
+    populateMenu();
+    activateListeners();
+  }
+
+  //populateMainSearch() populates the search section of the site. This is the default view on load and so
+  //function is called during page initialization as well as on nav click
+  function populateMainSearch() {
+    $(".main-content").empty();
+    $(".main-content")
+      .hide()
+      .append(
+        "<h1 id='site-header'>pitch<span><i class='fas fa-compact-disc'></i></span></h1>",
+        "<h6>An intuitive guide to help you navigate the world of music</h6>",
+        "<input type='text' class='searchTerm' id='input' placeholder='Enter Artist Name'/><br />",
+        "<a class='waves-effect waves-light btn-large search-btn'>Find Your Band!</a>"
+      )
+      .fadeIn(800);
+    //JD 9/30 Moved search function inside populateMainSearch() so that it also functions
+    // when navigated to not just on load
+    $(".search-btn").on("click", function (event) {
+      event.preventDefault();
+
+      //var currentArtistName stores user input
+      //NOTE: this var is declared and initially defined in the api-calls.js
+      currentArtistName = $(".searchTerm").val();
+      if (currentArtistName === "") {
+        return;
+      }
+      artistHistory.push(currentArtistName);
+      storeArtist();
+
+      //calling populateMainInfo()
+      callMusicBrainzAPI();
+      callYoutubeAPI();
+      populateMainInfo();
+      $("#input").val("");
+      console.log("history" + artistHistory);
+    });
+  }
+
+  //populates a YouTube player in the main-content space
+  function populateMainYoutube() {
+    callYoutubeAPI();
+    $(".main-content").empty();
+    populateMenu();
+    activateListeners();
+    $(".main-content").append(
+      "<br><br><iframe width='420' height='345' src='https://www.youtube.com/embed/" +
+        videoId +
+        "'></iframe>"
+    );
+  }
+
   //Appends nav icons to DOM
   function populateMenu() {
     $(".main-content")
@@ -47,43 +96,6 @@ $(document).ready(function () {
         "<i class='fas fa-search fa-3x' id='search-nav-btn'></i></a>"
       )
       .fadeIn(800);
-  }
-
-  //JD 9/29 Created populateMainSearch()
-  //populateMainSearch() populates the search section of the site. This is the default view on load and so
-  //function is called during page initialization as well as on nav click
-  function populateMainSearch() {
-    $(".main-content").empty();
-    $(".main-content")
-      .hide()
-      .append(
-        "<h1 id='site-header'>pitch<span><i class='fas fa-compact-disc'></i></span></h1>",
-        "<h6>An intuitive guide to help you navigate the world of music</h6>",
-        "<input type='text' class='searchTerm' id='input' placeholder='Enter Artist Name'/><br />",
-        "<a class='waves-effect waves-light btn-large search-btn'>Find Your Band!</a>"
-      )
-      .fadeIn(800);
-  }
-
-  //JD 9/29 Moved menu population into it's own function to dry out code
-  //populateMainInfo
-  //replaces search html with Info html. Also called from nav icons
-  function populateMainInfo() {
-    $(".main-content").empty();
-    wikipediaSearch();
-    populateMenu();
-    activateListeners();
-  }
-
-  function populateMainYoutube() {
-    callYoutubeAPI();
-    $(".main-content").empty();
-    populateMenu();
-    $(".main-content").append(
-      "<br><br><iframe width='420' height='345' src='https://www.youtube.com/embed/" +
-        videoId +
-        "'></iframe>"
-    );
   }
 
   function storeArtist() {
