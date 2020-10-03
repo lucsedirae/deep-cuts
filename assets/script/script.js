9a686869dd6a94ca91405015c4b5119c9c3bb976
Mark Major <mmajor@valcv.org>
2020-10-03 11:28:57 -0400

    nav bar works agian

--- assets/script/script.js
+++ assets/script/script.js
@@ -302,22 +302,21 @@ $(document).ready(function () {
 
 // THIS IS JAVACRIPT FOR THE NAV MENTI
 
-(function () {
-  var hamburger = {
-    navToggle: document.querySelector(".nav-toggle"),
-    nav: document.querySelector("nav"),
 
-    doToggle: function (e) {
+(function() {
+
+  var hamburger = {
+    navToggle: document.querySelector('.nav-toggle'),
+    nav: document.querySelector('nav'),
+
+    doToggle: function(e) {
       e.preventDefault();
-      this.navToggle.classList.toggle("expanded");
-      this.nav.classList.toggle("expanded");
-    },
+      this.navToggle.classList.toggle('expanded');
+      this.nav.classList.toggle('expanded');
+    }
   };
 
-  hamburger.navToggle.addEventListener("click", function (e) {
-    hamburger.doToggle(e);
-  });
-  hamburger.nav.addEventListener("click", function (e) {
-    hamburger.doToggle(e);
-  });
-});
+  hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+  // hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
+
+}());
